c13e3a7314048142a5f0e380c57cf804d64a96c0
usac201742012 <2777462210101@ingenieria.usac.edu.gt>
2020-06-22 20:52:24 -0600

    actualizacion

--- data_server/servidorp2.py
+++ data_server/servidorp2.py
@@ -6,7 +6,7 @@ import logging
 import time
 import os 
 import socket
-from brock_dt import *    #Informacion de la conexion
+from brok_dt import *    #Informacion de la conexion
 
 
 
@@ -22,43 +22,80 @@ class server():                   #verificaciones del servidor
         self.listaalives = []
         self.trama = ''
         self.LOG_FILENAME = 'mqtt.log'
+        self.client = mqtt.Client(clean_session=True) #Nueva instancia de cliente
 
 
     def start(self):
         qos = 2
-        client = mqtt.Client(clean_session=True) #Nueva instancia de cliente
-        client.subscribe([("comandos/22/201742012", qos), ("comandos/22/201701026", qos), ("comandos/22/201709532", qos), ("200", qos)])
-        client.on_connect = self.on_connect #Se configura la funcion "Handler" cuando suceda la conexion
-        client.on_message = self.on_message #Se configura la funcion "Handler" que se activa al llegar un mensaje a un topic subscrito
-        client.username_pw_set(MQTT_USER, MQTT_PASS) #Credenciales requeridas por el broker
-        client.connect(host=MQTT_HOST, port = MQTT_PORT) #Conectar al servidor remoto
-        
+    
+        self.client.subscribe([("comandos/22/201742012", qos), ("comandos/22/201701026", qos), ("comandos/22/201709532", qos), ("200", qos)])
+        self.client.on_connect = self.on_connect #Se configura la funcion "Handler" cuando suceda la conexion
+        self.client.on_message = self.on_message #Se configura la funcion "Handler" que se activa al llegar un mensaje a un topic subscrito
+        self.client.username_pw_set(MQTT_USER, MQTT_PASS) #Credenciales requeridas por el broker
+        self.client.connect(host=MQTT_HOST, port = MQTT_PORT) #Conectar al servidor remoto
+        self.client.publish("test", "Mensaje inicial", qos = 0, retain = False)
+
+
+    def publishData(self, topicRoot, topicName, value, qos = 0, retain = False):
+        topic = topicRoot + "/" + topicName
+        client.publish(topic, value, qos, retain)
+
+
 
     
+    '''
+    def __setupMQTTClient(self, address, port):
+
+        self.mqttC = mqttClient.Client(client_id="METEO"+str(self.getStationNumber())+"_METEO")
+		self.mqttC.on_message = self.__mqttCallback_onMessage
+		self.mqttC.on_connect = self.__mqttCallback_onConnect
+		self.mqttC.on_publish = self.__mqttCallback_onPublish
+
+		try:
+			self.mqttC.connect(address, port)
+
+		except socket.error:
+			
+			print ('Could not connect to Mosquitto broker at ' + str(address) + ':' + str(port))
+			print( 'Restarting service and trying to reconnect...')
+
+			os.system("sudo service mosquitto restart")
+			time.sleep(5)
+			self.mqttC.reconnect()
+
+    '''
+
+
+
     def on_connect(self, client, userdata, flags, rc): 
         connectionText = "CONNACK recibido del broker con codigo: " + str(rc)
         logging.info(connectionText)
 
-    #Handler en caso se publique satisfactoriamente en el broker MQTT
-    def on_publish(self, client, userdata, mid): 
+
+   
+    def on_publish(self, client, userdata, mid):           #Handler en caso se publique satisfactoriamente en el broker MQTT
         publishText = "Publicacion satisfactoria"
         logging.debug(publishText)
+
 
 
     def agregar(self, codigo):               #agrega usuarios a la lista de ALIVES
         self.listaalives.append(codigo)
 
+
+
     def elimin(self, codigo):
         self.listaalive.remove(codigo)       #elimina usuarios de la lista de ALIVES
 
-    #Callback que se ejecuta cuando llega un mensaje al topic suscrito
-    def on_message(self, client, userdata, msg):
-        #Se muestra en pantalla informacion que ha llegado
-        logging.info("Ha llegado el mensaje al topic: " + str(msg.topic))
+    
+
+    def on_message(self, client, userdata, msg):                       #Callback que se ejecuta cuando llega un mensaje al topic suscrito
+        
+        logging.info("Ha llegado el mensaje al topic: " + str(msg.topic))   #Se muestra en pantalla informacion que ha llegado
         logging.info("El contenido del mensaje es: " + str(msg.payload))
     
-        # se almacena en el log 
-        logCommand = 'echo "(' + str(msg.topic) + ') -> ' + str(msg.payload) + '" >> ' + self.LOG_FILENAME
+        
+        logCommand = 'echo "(' + str(msg.topic) + ') -> ' + str(msg.payload) + '" >> ' + self.LOG_FILENAME   # se almacena en el log 
         os.system(logCommand)
         trama = msg.payload
 
@@ -79,6 +116,7 @@ class server():                   #verificaciones del servidor
             resultado = self.NO(trama[2:])
         return resultado
    
+
     def FTR(self, trama):                         #gestiona la solicitud de envio de archivos
 
         usuario = trama.split('$')[0]
@@ -102,6 +140,8 @@ class server():                   #verificaciones del servidor
 
         else:
             return '07' + usuario
+
+
 
 
     def ALIVE(self, trama):                  #corrobora que el cliente este conectado
@@ -128,8 +168,34 @@ class server():                   #verificaciones del servidor
     def NO(self, trama):                     #notifica de negacion a una solicitud
         print('solicitud de transferencia de archivo')
           
-objeto = server()  
-objeto.start()
 
 
-#si aparece este cambio jona?
+try:
+    while True:
+
+
+        #Para temperatura
+        for i in range(sensores.getSensorCount()):
+            publishData(SENSORES, (str(i) + "/" + TEMPERATURA), sensores.getTemperatura(i))
+            
+        
+        #Para humedad
+        for i in range(sensores.getSensorCount()):
+            publishData(SENSORES, (str(i) + "/" + HUMEDAD), sensores.getHumedad(i))
+
+        #Para presion
+        for i in range(sensores.getSensorCount()):
+            publishData(SENSORES, (str(i) + "/" + PRESION_A), sensores.getPresionA(i))
+
+
+        logging.info("Los datos han sido enviados al broker")            
+
+        #Retardo hasta la proxima publicacion de info
+        time.sleep(DEFAULT_DELAY)
+
+except KeyboardInterrupt:
+    logging.warning("Desconectando del broker MQTT...")
+
+finally:
+    client.disconnect()
+    logging.info("Se ha desconectado del broker. Saliendo...")
